096c412a7d595828948b537b00689321b5be4fdf
eja <90547944+MasReza354@users.noreply.github.com>
2024-01-07 18:36:14 +0700

    Add files via upload

---
+++ src/pages/index.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="page-layout">
+    <PageBackground/>
+    <div class="card">
+      <div class="profile-picture">
+        <img
+          src="https://github.com/MasReza354.png"
+          alt="maseja"
+          class="profile-pic"
+        />
+      </div>
+      <div class="profile-area">
+        <h1 class="title">Reza Putra Nurhudaya</h1>
+        <p class="description">IT Support • Informatics Engineering • NOC Engineering</p>
+      </div>
+      <div class="tabs">
+        <div
+          v-for="tab in tabs"
+          :key="tab.id"
+          :class="['tab', { active: activeTab === tab.id }]"
+          @click="activeTab = tab.id"
+        >
+          <span>{{ tab.name }}</span>
+        </div>
+      </div>
+      <Links v-if="activeTab === 'links'"/>
+      <Projects v-else/>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Links from "@components/Links.vue";
+import Projects from "@components/Projects.vue";
+import PageBackground from "@components/PageBackground.vue";
+
+const tabs = [
+  {
+    id: "links",
+    name: "Social Media",
+  },
+  {
+    id: "projects",
+    name: "Code Editor",
+  },
+];
+
+const activeTab = ref("links");
+</script>
+
+<style lang="scss" scoped>
+.page-layout {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow: hidden;
+  background: var(--color-background);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    background: var(--color-secondary);
+    border-radius: 10px;
+    border: 1px solid var(--color-border);
+    width: 90%;
+    max-width: 400px;
+    z-index: 1;
+  }
+  .profile-picture {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    overflow: hidden;
+    align-self: center;
+    margin: 20px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .profile-area {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 10px;
+
+    .title {
+      font-size: 1.2rem;
+      font-weight: 500;
+      padding: 0px 0 5px 0;
+    }
+
+    .description {
+      font-size: 0.8rem;
+      color: var(--color-font-light);
+    }
+  }
+
+  .tabs {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid var(--color-border);
+
+    .tab {
+      padding: 10px 15px;
+      border-bottom: 2px solid transparent;
+      opacity: 0.6;
+      border-bottom: 2px solid transparent;
+
+      span {
+        font-weight: 500;
+      }
+      &.active {
+        border-bottom: 2px solid var(--color-primary);
+        opacity: 1;
+      }
+
+      &:hover {
+        cursor: pointer;
+        opacity: 1;
+        transition: 0.2s all ease-in-out;
+      }
+    }
+  }
+}
+</style>
